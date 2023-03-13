a2179dbfd467fa5ff5adbe03372e15380f0ae365
GuiCT <gtomiasi@gmail.com>
2023-03-13 20:59:43 -0300

    initializing Maven Project

---
+++ src/main/java/Trabalho1Java.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author guilherme
+ */
+public class Trabalho1Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
